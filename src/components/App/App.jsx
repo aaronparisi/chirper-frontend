bc029c0d2434bb571bdb98d2fb575cbfc4755c90
Aaron <parisi.aaron@gmail.com>
2021-05-03 21:12:39 +0000

    create About component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import HomeContainer from '../Home/HomeContainer';
 import HelpContainer from '../Help/HelpContainer';
+import AboutContainer from '../About/AboutContainer';
 
 function App() {
   return (
     <React.Fragment >
       <Route exact path="/" component={HomeContainer} />
-      <Route path="/help" component={HelpContainer} />
+      <Route exact path="/help" component={HelpContainer} />
+      <Route exact path="/about" component={AboutContainer} />
     </React.Fragment>
   );
 }
